03c2d2031418661f6adc48ba80bbbceb19759ec4
Dan Lyons <daniel.lyons178@live.com>
2021-02-25 19:58:07 +0000

    Tidy runner.py for client

--- scraper/client/runner.py
+++ scraper/client/runner.py
@@ -1,17 +1,15 @@
 import importlib
 import argparse
+from scraper.client.lib.link_extraction.link_extractor import LinkExtractor
 from scraper.client.lib.body_reader.body_reader import BodyReader
 from scraper.client.link_writer import LinkWriter
 from scraper.common.comms.rabbit.rabbit_outputter import RabbitOutputter
 from scraper.common.comms.rabbit.rabbit_reciever import RabbitReciever
-from scraper.client.lib.link_extraction.link_extractor import LinkExtractor
 from scraper.common.comms.rabbit.rabbit_helpers import (
     rabbit_blocking_connection_factory,
 )
 
-
 def main():
-
     parser = argparse.ArgumentParser(description="Distributed Scraper")
     subs = parser.add_subparsers(dest="mode")
 
@@ -34,6 +32,7 @@ def main():
     elif args.mode == "body":
         run_result_processor(args)
 
+
 def add_modes(subs):
     link_extractor = subs.add_parser("links")
     link_extractor.add_argument("--extractors", "-ex")
@@ -43,68 +42,63 @@ def add_modes(subs):
 
 
 def run_link_extractor(args):
-
-    classes = args.extractors.split(",")
-
-    splits = [clazz.split(".") for clazz in classes]
-    module_classes = [(split[:-1], split[-1]) for split in splits]
-
-    conn_factory = rabbit_blocking_connection_factory(
-        args.rabbit_host,
-        args.rabbit_port,
-        (args.rabbit_user, args.rabbit_password),
-    )
+    conn_factory = _get_rabbit_conn_factory(args)
 
     link_outputter = RabbitOutputter(("links", "direct"), conn_factory)
     link_writer = LinkWriter(link_outputter)
 
-    for (module_str, clazz_str) in module_classes:
-        module_str = ".".join(module_str)
-        module = importlib.import_module(module_str)
-        clazz = getattr(module, clazz_str)
-
-        if not issubclass(clazz, LinkExtractor):
-            raise Exception("Must subclass LinkExtractor")
-
-        ex = clazz(link_writer)
+    for clazz in _get_constructors(args.extractors, LinkExtractor):
+        link_extractor = clazz(link_writer)
         rec = RabbitReciever(
             conn_factory,
             ("result", "topic"),
-            f"{module_str}.{clazz_str}",
-            ex.for_links_pattern(),
+            _queue_name_from_class(clazz),
+            link_extractor.for_links_pattern(),
         )
-        ex.register_receiver(rec)
-        ex.run()
+        link_extractor.register_receiver(rec)
+        link_extractor.run()
 
 
 def run_result_processor(args):
-    classes = args.body_readers.split(",")
-    splits = [clazz.split(".") for clazz in classes]
-    module_classes = [(split[:-1], split[-1]) for split in splits]
+    conn_factory = _get_rabbit_conn_factory(args)
 
-    conn_factory = rabbit_blocking_connection_factory(
+    for clazz in _get_constructors(args.body_readers, BodyReader):
+        body_reader = clazz()
+        rec = RabbitReciever(
+            conn_factory,
+            ("result", "topic"),
+            _queue_name_from_class(clazz),
+            body_reader.for_links_pattern(),
+        )
+        body_reader.register_receiver(rec)
+        body_reader.run()
+
+
+def _queue_name_from_class(clazz):
+    return f"{clazz.__module__}.{clazz.__name__}"
+
+
+def _get_rabbit_conn_factory(args):
+    return rabbit_blocking_connection_factory(
         args.rabbit_host,
         args.rabbit_port,
         (args.rabbit_user, args.rabbit_password),
     )
 
+def _get_constructors(class_string, super_class):
+    classes = class_string.split(",")
+    splits = [clazz.split(".") for clazz in classes]
+    module_classes = [(split[:-1], split[-1]) for split in splits]
+
     for (module_str, clazz_str) in module_classes:
         module_str = ".".join(module_str)
         module = importlib.import_module(module_str)
         clazz = getattr(module, clazz_str)
 
-        if not issubclass(clazz, BodyReader):
-            raise Exception("Must subclass LinkExtractor")
+        if not issubclass(clazz, super_class):
+            raise Exception(f"Must subclass {super_class.__name__}")
 
-        body_reader = clazz()
-        rec = RabbitReciever(
-            conn_factory,
-            ("result", "topic"),
-            f"{module_str}.{clazz_str}",
-            body_reader.for_links_pattern(),
-        )
-        body_reader.register_receiver(rec)
-        body_reader.run()
+        yield clazz
 
 
 if __name__ == "__main__":
--- scraper/common/comms/rabbit/rabbit_helpers.py
+++ scraper/common/comms/rabbit/rabbit_helpers.py
@@ -1,10 +1,10 @@
 import pika
-from pika.connection import Connection
-
 
 def rabbit_blocking_connection_factory(host, port, creds):
     creds = pika.PlainCredentials(creds[0], creds[1])
     params = pika.ConnectionParameters(host, port, credentials=creds)
+
     def factory():
-        return pika.BlockingConnection(params) 
+        return pika.BlockingConnection(params)
+
     return factory
